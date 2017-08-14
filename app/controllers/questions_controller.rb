a7aedd66aed08917de5cf8756424e84b70069a92
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-14 13:09:19 +0200

    added answer method with params instance variale

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,18 @@
 class QuestionsController < ApplicationController
   def ask
+    # @your_message = params[:query]
   end
 
+
   def answer
+    @your_message = params[:query]
+    if @your_message && @your_message.downcase == "i am going to work right now!"
+       @coach_response = ""
+    elsif @your_message && @your_message.end_with?("?")
+      @coach_response = "Silly question, get dressed and go to work!"
+    else
+     @coach_response = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
